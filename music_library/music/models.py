8619dbd6c0e1f69c311f008dfaf3a675d7d193ee
unknown <cferrara212@gmail.com>
2021-11-09 08:10:59 -0700

    added genre to models, updated serializers and urls.

--- music_library/music/models.py
+++ music_library/music/models.py
@@ -7,6 +7,7 @@ class Song(models.Model):
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
     release_date = models.DateTimeField(auto_now_add=True)
+    genre = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.title
